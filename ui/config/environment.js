8ca0c0fc6f27eebdd911566733a5eb46f8766a8a
Kevin Hinterlong <hinterlong@verizonmedia.com>
2021-04-27 15:47:31 -0500

    build: update navi to 0.2.0-beta.612

--- ui/config/environment.js
+++ ui/config/environment.js
@@ -30,7 +30,7 @@ module.exports = function (environment) {
       dataEpoch: '1900-01-01',
       FEATURES: {
         enableDashboardFilters: true,
-        exportFileTypes: [],
+        exportFileTypes: ['csv'],
         enableTableEditing: true,
         enableTotals: true,
       },
