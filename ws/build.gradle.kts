4f1cac14c23a2f74c64052bf183ec031369b6384
anupkumangodan <45950966+anupkumangodan@users.noreply.github.com>
2021-04-28 08:48:22 -0700

    build: move to demo resources to jar

--- ws/build.gradle.kts
+++ ws/build.gradle.kts
@@ -20,6 +20,8 @@ repositories {
 
 dependencies {
     implementation(project(":ui"))
+    //Comment this line to disable the demo config
+    implementation("dev.yavin","demo-config","0.9")
     implementation("com.yahoo.navi", "models", "0.2.0-beta-SNAPSHOT") {
         exclude(group = "com.yahoo.elide", module = "elide-core")
     }
