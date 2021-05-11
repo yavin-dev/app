b9b74c7d5035b722558db3e5137c322f162145c0
Anup Kumangodan <anupkp@verizonmedia.com>
2021-05-11 15:36:28 -0700

    initial

---
+++ yavin-run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -xe
+artifact=${1:-"app"}
+version=$(curl -s http://search.maven.org/solrsearch/select?q=g:"dev.yavin"+AND+a:"${artifact}" | tr "," "\n" | awk -F'[":]' '/"latestVersion"/{print $5}')
+
+curl https://repo1.maven.org/maven2/dev/yavin/${artifact}/${version}/${artifact}-${version}.jar -o ${artifact}-${version}.jar
+java -jar ${artifact}-${version}.jar
