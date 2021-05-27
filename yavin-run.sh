193c430225b752ad2cd8b8c32151ef785bd5fd4b
Anup Kumangodan <anupkp@verizonmedia.com>
2021-05-27 12:43:17 -0700

    bake in jar during docker image build

--- yavin-run.sh
+++ yavin-run.sh
@@ -3,9 +3,11 @@
 set -xe
 artifact=${1:-"yavin-app"}
 profile_opt=${2:-''}
+download_only=${3:-''}
+download_loc="/usr/local/lib/"
 version=$(curl -s http://search.maven.org/solrsearch/select?q=g:"dev.yavin"+AND+a:"${artifact}" | tr "," "\n" | awk -F'[":]' '/"latestVersion"/{print $5}')
 
-curl https://repo1.maven.org/maven2/dev/yavin/${artifact}/${version}/${artifact}-${version}.jar -o ${artifact}-${version}.jar
-cmd="java -jar ${artifact}-${version}.jar"
+curl https://repo1.maven.org/maven2/dev/yavin/${artifact}/${version}/${artifact}-${version}.jar -o ${download_loc}/${artifact}-${version}.jar
+cmd="java -jar ${download_loc}/${artifact}-${version}.jar"
 [[ ! -z ${profile_opt} ]] && cmd="${cmd} --spring.profiles.active=${profile_opt}"
-echo ${cmd} && eval ${cmd}
+[[ -z ${download_only} ]] && echo ${cmd} && eval ${cmd}
